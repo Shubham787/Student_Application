8ff7e4a927b1d570646a0cfe7a9a8d08afc910ae
Shubham787 <shubhamyamgekar01@gmail.com>
2025-06-29 12:17:11 +0530

    hardcoded frontendURL

--- SA_Backend/src/server.ts
+++ SA_Backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://student-application-six.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
