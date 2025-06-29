237570f560c1bf5579b536d4514e85b7f2d5002c
Shubham787 <shubhamyamgekar01@gmail.com>
2025-06-29 12:22:36 +0530

    Fixed deployment issues

--- SA_Backend/src/server.ts
+++ SA_Backend/src/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://student-application-six.vercel.app/",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
